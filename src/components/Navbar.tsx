9ef1a61dc4f3b9e13d16150377772b376579056b
laxman200456 <laxmancsuthar2004@gmail.com>
2025-01-23 09:34:00 +0530

    navbar problem solved

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -92,27 +92,6 @@ const Header: React.FC = () => {
               {link.name}
             </Link>
           ))}
-
-          {/* Close Button */}
-          <button
-            onClick={closeMenu}
-            className="text-gray-700 focus:outline-none mt-8"
-          >
-            <svg
-              className="w-8 h-8"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
         </div>
       )}
     </header>
